5b056ee7ddc6a3d89161cdeb210567ca3a59edae
gergocs <csgergo92@gmail.com>
2021-04-15 10:31:19 +0200

    bug fix

--- globals.php
+++ globals.php
@@ -1,9 +1,4 @@
 <?php
-ini_set('session.cookie_secure', 1);
-ini_set('session.cookie_httponly', 1);
-ini_set('session.use_only_cookies', 1);
-if(!session_id()) session_start();
-
 if(!isset($_SESSION['gLoggedIn'])){
     $_SESSION['gLoggedIn'] = false;
 }
